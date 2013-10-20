935f36988e57a1de0b67d187263fd60230a66ad3
Danilo Andjelic <daniloandj@gmail.com>
2013-10-20 12:48:43 +0200

    First test commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject commit-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+				[com.novemberain/monger "1.6.0"]
+                 [lein-pprint "1.1.1"]]
+				 
+	:plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
+				 )
---
+++ test/commit_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns commit-test.core-test
+  (:require [clojure.test :refer :all]
+            [commit-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
